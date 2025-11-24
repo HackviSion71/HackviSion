e8fab7a1da8598860ec8bbefba85312cd887071b
HackviSion71 <hackvision71@gmail.com>
2025-11-24 21:28:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/HackviSion/",   // 👉 yaha EXACT tumhare GitHub repo ka naam
+  base: "/",   // 👉 yaha EXACT tumhare GitHub repo ka naam
   plugins: [react()],
 })
 
